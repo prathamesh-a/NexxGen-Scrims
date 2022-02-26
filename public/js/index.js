d638143407a97efa9eba6c9dc2e045eb8571dcb4
prathamesh-a <89336149+prathamesh-a@users.noreply.github.com>
2022-02-26 14:07:03 +0530

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -21,16 +21,19 @@ document.getElementById("feedback_form").addEventListener("submit",(event)=>{
 function feedBackSend(){
     const email = document.getElementById("feedback_user_email").value;
     const text = document.getElementById("feedback_user_text").value;
-    db.collection("feedBack").add({
-        email: email,
-        message: text,
-    })
-    .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-        document.getElementById("feedback_user_email").value = " ";
-        document.getElementById("feedback_user_text").value = "";
-    })
-    .catch((error) => {
-        console.error("Error adding document: ", error);
-    });
+    if (email !== "" && text !== ""){
+        db.collection("feedBack").add({
+            email: email,
+            message: text,
+        })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            document.getElementById("feedback_user_email").value = " ";
+            document.getElementById("feedback_user_text").value = "";
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+    }
+    else window.alert("Fill empty fields.")
 }
